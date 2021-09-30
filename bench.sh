8a0f8e3da8bd6d8f4a1d703fbbdba04934ef4006
Nick Tehrany <nicktehrany1@gmail.com>
2021-09-30 11:41:03 +0000

    Updated bench script

--- bench.sh
+++ bench.sh
@@ -12,15 +12,20 @@ DIR=$1
 CONFIG=$2.dat
 
 mkdir -p results
+# Clean in case old results are there
+rm results/{Fillseq,Fillrand,Readseq,Readrand}-$CONFIG
 
 CMD="$DB_BENCH --db=$DIR --benchmarks=fillseq,fillrandom,readseq,readrandom --key_size=16 --value_size=100 --num=10000000 --reads=1000000 --use_direct_reads --use_direct_io_for_flush_and_compaction --compression_type=none"
 
+# TODO: seperate cmd for config 4 with zenfs rocksdb plugin
+#CMD="$DB_BENCH --db=$DIR --benchmarks=fillseq,fillrandom,readseq,readrandom --key_size=16 --value_size=100 --num=10000000 --reads=1000000 --use_direct_reads --use_direct_io_for_flush_and_compaction --compression_type=none"
+
 echo "micros/op ops/sec MB/s" | tee Fillseq-$CONFIG Fillrand-$CONFIG Readseq-$CONFIG Readrand-$CONFIG > /dev/null
 
-for ((i = 0 ; i <= $ITERS ; i++)); do
-    RESULT="$(CMD)"
-    echo $RESULT | grep "fillseq" | awk '{print $3,$5,$7}' >> results/Fillseq-$CONFIG
-    echo $RESULT | grep "fillrand" | awk '{print $3,$5,$7}' >> results/Fillrand-$CONFIG
-    echo $RESULT | grep "readseq" | awk '{print $3,$5,$7}' >> results/Readseq-$CONFIG
-    echo $RESULT | grep "readrand" | awk '{print $3,$5,$7}' >> results/Readrand-$CONFIG
+for ((i = 0 ; i < $ITERS ; i++)); do
+    RESULT=$($CMD)
+    echo "${RESULT}" | grep "fillseq" | awk '{print $3,$5,$7}' >> results/Fillseq-$CONFIG
+    echo "${RESULT}" | grep "fillrand" | awk '{print $3,$5,$7}' >> results/Fillrand-$CONFIG
+    echo "${RESULT}" | grep "readseq" | awk '{print $3,$5,$7}' >> results/Readseq-$CONFIG
+    echo "${RESULT}" | grep "readrand" | awk '{print $3,$5,$7}' >> results/Readrand-$CONFIG
 done

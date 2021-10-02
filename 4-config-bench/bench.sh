aa46554b1e15799c70fcb781b1dcd505dbcdf518
Nick <nicktehrany1@gmail.com>
2021-10-02 11:36:51 +0200

    Bench.sh added datadir var

--- 4-config-bench/bench.sh
+++ 4-config-bench/bench.sh
@@ -13,9 +13,12 @@ DB_BENCH=$(find $HOME | grep "rocksdb/db_bench$")
 DIR=$1
 CONFIG=$2.dat
 
-mkdir -p results
-# Clean in case old results are there
-rm -f results/{Fillseq,Fillrand,Overwrite,Updaterandom,Readseq,Readrand}-$CONFIG
+# Change variable to write data to different directory
+DATADIR="data"
+
+mkdir -p $DATADIR
+# Clean in case old data are there
+rm -f $DATADIR/{Fillseq,Fillrand,Overwrite,Updaterandom,Readseq,Readrand}-$CONFIG
 
 if [ "$2" = "config-4" ]; then
     CMD="sudo $DB_BENCH --fs_uri=zenfs://dev:$DIR --benchmarks=fillseq,fillrandom,overwrite,updaterandom,readseq,readrandom --key_size=16 --value_size=100 --num=1000000 --reads=100000 --use_direct_reads --use_direct_io_for_flush_and_compaction --compression_type=none"
@@ -25,10 +28,10 @@ fi
 
 for ((i = 0 ; i < $ITERS ; i++)); do
     RESULT=$($CMD)
-    echo "${RESULT}" | grep "fillseq" | awk '{print $3,$5,$7}' >> results/Fillseq-$CONFIG
-    echo "${RESULT}" | grep "fillrand" | awk '{print $3,$5,$7}' >> results/Fillrand-$CONFIG
-    echo "${RESULT}" | grep "overwrite" | awk '{print $3,$5,$7}' >> results/Overwrite-$CONFIG
-    echo "${RESULT}" | grep "updaterandom" | awk '{print $3,$5,$7}' >> results/Updaterandom-$CONFIG
-    echo "${RESULT}" | grep "readseq" | awk '{print $3,$5,$7}' >> results/Readseq-$CONFIG
-    echo "${RESULT}" | grep "readrand" | awk '{print $3,$5,$7}' >> results/Readrand-$CONFIG
+    echo "${RESULT}" | grep "fillseq" | awk '{print $3,$5,$7}' >> $DATADIR/Fillseq-$CONFIG
+    echo "${RESULT}" | grep "fillrand" | awk '{print $3,$5,$7}' >> $DATADIR/Fillrand-$CONFIG
+    echo "${RESULT}" | grep "overwrite" | awk '{print $3,$5,$7}' >> $DATADIR/Overwrite-$CONFIG
+    echo "${RESULT}" | grep "updaterandom" | awk '{print $3,$5,$7}' >> $DATADIR/Updaterandom-$CONFIG
+    echo "${RESULT}" | grep "readseq" | awk '{print $3,$5,$7}' >> $DATADIR/Readseq-$CONFIG
+    echo "${RESULT}" | grep "readrand" | awk '{print $3,$5,$7}' >> $DATADIR/Readrand-$CONFIG
 done
